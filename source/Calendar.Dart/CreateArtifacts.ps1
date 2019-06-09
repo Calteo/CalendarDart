5d131a6e875e155b294f86712fd863faeb376892
Calteo <matthias@famger.de>
2019-06-09 21:04:47 +0200

    package of games and resource assemblies included

--- source/Calendar.Dart/CreateArtifacts.ps1
+++ source/Calendar.Dart/CreateArtifacts.ps1
@@ -13,6 +13,11 @@ if (!(Test-Path -path $destinationFolder)) {$rc = New-Item $destinationFolder -T
 Write-Output $destinationFolder
 Copy-Item bin\Release\*.exe -Destination $destinationFolder -Force -PassThru | ForEach-Object { Write-Output "--> $($_.Name)" }
 Copy-Item bin\Release\*.exe.config -Destination $destinationFolder -Force -PassThru | ForEach-Object { Write-Output "--> $($_.Name)" }
-Copy-Item bin\Release\*.dll -Exclude "System.*" -Destination $destinationFolder -Force -PassThru | ForEach-Object { Write-Output "--> $($_.Name)" }
+Copy-Item bin\Release\*.dll -Exclude "System.*" -Destination $destinationFolder -Force -Container -PassThru | ForEach-Object { Write-Output "--> $($_.Name)" }
+
+# add supported languages here
+Copy-Item bin\Release\de -filter *.dll -Recurse -Destination $destinationFolder -Force -Container -PassThru | ForEach-Object { Write-Output "--> $($_.Name)" }
+
+Copy-Item Games -Recurse -Filter *.xml -Destination $destinationFolder -Force -PassThru | ForEach-Object { Write-Output "--> $($_.Name)" }
 
 Pop-Location
